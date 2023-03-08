786f4925664e8384072d61fad72095f5b9c03429
Siem-P <64197688+Siem-P@users.noreply.github.com>
2023-03-08 15:06:24 +0100

    Setting up the foundation

---
+++ index.js
@@ -0,0 +1,40 @@
+
+// Importeer express uit de node_modules map
+import express, { json, urlencoded } from "express";
+import bodyParser from "body-parser";
+
+// API URL
+const url = "https://whois.fdnd.nl/api/v1/squad/";
+
+let data = fetch('./public/api/game/943.json')
+    .then(response => response.json())
+    .then((json) => setData(json))
+
+    
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "./view");
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+
+  res.render("index", data);
+});
+
+app.set("port", process.env.PORT || 8000);
+
+app.listen(app.get("port"), function () {
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
+
+// Function for fetching the API
+async function fetchApi(url) {
+  const data = await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
+  return data;
+}
