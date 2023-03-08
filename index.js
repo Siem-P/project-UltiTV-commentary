3e41ad4b7ba7bb5b43b50f1da511699110634fa0
Siem-P <64197688+Siem-P@users.noreply.github.com>
2023-03-08 15:45:19 +0100

    Json file reader script

--- index.js
+++ index.js
@@ -2,15 +2,23 @@
 // Importeer express uit de node_modules map
 import express, { json, urlencoded } from "express";
 import bodyParser from "body-parser";
+import fs from "fs"
 
 // API URL
 const url = "https://whois.fdnd.nl/api/v1/squad/";
 
-let data = fetch('./public/api/game/943.json')
-    .then(response => response.json())
-    .then((json) => setData(json))
+let jsonData = fs.readFile('./public/api/game/943.json', 'utf8', handleJsonData);
 
-    
+function handleJsonData(err, data) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  let jsonData = JSON.parse(data);
+}
+
+console.log(jsonData);
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -21,8 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-
-  res.render("index", data);
+  res.render("index");
 });
 
 app.set("port", process.env.PORT || 8000);
